ccc4d0394e8bbe5c68b103c6a7808801e41892ee
Junil Ko <goya@mocapot.com>
2021-07-17 09:22:19 +0900

    프로젝트 시작

---
+++ ChooseTop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChooseTop
+//
+//  Created by wannabewize on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
