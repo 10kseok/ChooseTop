587d95409410ae63c61e6e9f032ad835a604888f
10kseok <76582376+10kseok@users.noreply.github.com>
2021-07-24 22:37:51 +0900

    ChooseTop quiz update
    해당 내용에 맞게 풀이한 코드입니다.

--- ChooseTop/ViewController.swift
+++ ChooseTop/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var random3IntSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var random3Int = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func generateRandom3Int(_ sender: Any) {
+        random3IntSegmentedControl.removeAllSegments()
+        guard random3IntSegmentedControl.numberOfSegments == 0 else { return }
+        
+        var tempRandomIntSet = Set<Int>()
+        
+        while tempRandomIntSet.count < 3 {
+            tempRandomIntSet.insert(Int.random(in: 1...100))
+        }
+        
+        random3Int = Array(tempRandomIntSet)
+        
+        for int in random3Int {
+            random3IntSegmentedControl.insertSegment(withTitle: "\(int)", at: int, animated: true)
+        }
+        
+    }
+    
+    
+    @IBAction func checkAnswer(_ sender: Any) {
+        let selectedIndex = random3IntSegmentedControl.selectedSegmentIndex
+        var maxValueIndex: Int = 0
+        
+        for (idx, value) in random3Int.enumerated() {
+            if value == random3Int.max() {
+                maxValueIndex = idx
+            }
+        }
+        
+        let resultText =  (selectedIndex == maxValueIndex) ? "성공" : "실패"
+        resultLabel.text = resultText
+        
+        
+    }
+    
 
 }
 
